40357c92cb7b95b93e27a3c33ed5dae3df211c2a
Jimmy <shuu.ro.zl@gmail.com>
2012-06-19 22:14:32 +0800

    new created

---
+++ ebook/src/com/easyhome/ebook/BookPageFactory.java
@@ -0,0 +1,344 @@
+
+package com.easyhome.ebook;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.text.DecimalFormat;
+import java.util.Vector;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.preference.PreferenceManager;
+
+public class BookPageFactory {
+
+    private File book_file;
+    private MappedByteBuffer m_mbBuf;
+    private int m_mbBufLen;
+    private int m_mbBufBegin;
+    private int m_mbBufEnd;
+    private String m_strCharsetName = "GBK";
+    private Bitmap m_book_bg;
+    private int mWidth;
+    private int mHeight;
+    private Vector<String> m_lines = new Vector<String>();
+    private int m_fontSize = 26;
+    private int m_textColor = Color.BLACK;
+    private int m_backColor = 0xffff9e85;
+    private int marginWidth = 15;
+    private int marginHeight = 20;
+    private int mLineCount;
+    private float mVisibleHeight;
+    private float mVisibleWidth;
+    private boolean m_isfirstPage;
+    private boolean m_islastPage;
+    private Paint mPaint;
+    private SharedPreferences sharedPreferences;
+    DecimalFormat df = new DecimalFormat("##.##");
+    private Context mContext;
+    private int mPercent;
+
+    public BookPageFactory(int w, int h, Context context) {
+        mContext = context;
+        sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(mContext);
+        mWidth = w;
+        mHeight = h - sharedPreferences.getInt("PaddingButtom", 0);;
+        m_fontSize = sharedPreferences.getInt("font_size", 26);
+        m_textColor = sharedPreferences.getInt("font_color", Color.BLACK);
+        m_backColor = sharedPreferences.getInt("back_color", 0xffff9e85);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setTextAlign(Align.LEFT);
+        mPaint.setTextSize(m_fontSize);
+        mPaint.setColor(m_textColor);
+        mVisibleWidth = mWidth - marginWidth * 2;
+        mVisibleHeight = mHeight - marginHeight * 2;
+        mLineCount = (int) (mVisibleHeight / m_fontSize);
+        m_mbBufBegin = sharedPreferences.getInt("Begin", 0);
+        m_mbBufEnd = m_mbBufBegin;
+    }
+
+    public void setFontSize(int size) {
+        Editor edit = sharedPreferences.edit();
+        edit.putInt("font_size", size);
+        edit.commit();
+    }
+
+    public void setBackColor(int color) {
+        Editor edit = sharedPreferences.edit();
+        edit.putInt("back_color", color);
+        edit.commit();
+    }
+
+    public void openbook(File file) throws IOException {
+        book_file = file;
+        long lLen = book_file.length();
+        m_mbBufLen = (int) lLen;
+        m_mbBuf = new RandomAccessFile(book_file, "r").getChannel().map(
+                FileChannel.MapMode.READ_ONLY, 0, lLen);
+    }
+
+    protected byte[] readParagraphBack(int nFromPos) {
+        int nEnd = nFromPos;
+        int i;
+        byte b0, b1;
+        if (m_strCharsetName.equals("UTF-16LE")) {
+            i = nEnd - 2;
+            while (i > 0) {
+                b0 = m_mbBuf.get(i);
+                b1 = m_mbBuf.get(i + 1);
+                if (b0 == 0x0a && b1 == 0x00 && i != nEnd - 2) {
+                    i += 2;
+                    break;
+                }
+                i--;
+            }
+
+        } else if (m_strCharsetName.equals("UTF-16BE")) {
+            i = nEnd - 2;
+            while (i > 0) {
+                b0 = m_mbBuf.get(i);
+                b1 = m_mbBuf.get(i + 1);
+                if (b0 == 0x00 && b1 == 0x0a && i != nEnd - 2) {
+                    i += 2;
+                    break;
+                }
+                i--;
+            }
+        } else {
+            i = nEnd - 1;
+            while (i > 0) {
+                b0 = m_mbBuf.get(i);
+                if (b0 == 0x0a && i != nEnd - 1) {
+                    i++;
+                    break;
+                }
+                i--;
+            }
+        }
+        if (i < 0)
+            i = 0;
+        int nParaSize = nEnd - i;
+        int j;
+        byte[] buf = new byte[nParaSize];
+        for (j = 0; j < nParaSize; j++) {
+            buf[j] = m_mbBuf.get(i + j);
+        }
+        return buf;
+    }
+
+    protected byte[] readParagraphForward(int nFromPos) {
+        int nStart = nFromPos;
+        int i = nStart;
+        byte b0, b1;
+        if (m_strCharsetName.equals("UTF-16LE")) {
+            while (i < m_mbBufLen - 1) {
+                b0 = m_mbBuf.get(i++);
+                b1 = m_mbBuf.get(i++);
+                if (b0 == 0x0a && b1 == 0x00) {
+                    break;
+                }
+            }
+        } else if (m_strCharsetName.equals("UTF-16BE")) {
+            while (i < m_mbBufLen - 1) {
+                b0 = m_mbBuf.get(i++);
+                b1 = m_mbBuf.get(i++);
+                if (b0 == 0x00 && b1 == 0x0a) {
+                    break;
+                }
+            }
+        } else {
+            while (i < m_mbBufLen) {
+                b0 = m_mbBuf.get(i++);
+                if (b0 == 0x0a) {
+                    break;
+                }
+            }
+        }
+        int nParaSize = i - nStart;
+        byte[] buf = new byte[nParaSize];
+        for (i = 0; i < nParaSize; i++) {
+            buf[i] = m_mbBuf.get(nFromPos + i);
+        }
+        return buf;
+    }
+
+    protected Vector<String> pageDown() {
+        String strParagraph = "";
+        Vector<String> lines = new Vector<String>();
+        while (lines.size() < mLineCount && m_mbBufEnd < m_mbBufLen) {
+            byte[] paraBuf = readParagraphForward(m_mbBufEnd);
+            m_mbBufEnd += paraBuf.length;
+            try {
+                strParagraph = new String(paraBuf, m_strCharsetName);
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            String strReturn = "";
+            if (strParagraph.indexOf("\r\n") != -1) {
+                strReturn = "\r\n";
+                strParagraph = strParagraph.replaceAll("\r\n", "");
+            } else if (strParagraph.indexOf("\n") != -1) {
+                strReturn = "\n";
+                strParagraph = strParagraph.replaceAll("\n", "");
+            }
+
+            if (strParagraph.length() == 0) {
+                lines.add(strParagraph);
+            }
+            while (strParagraph.length() > 0) {
+                int nSize = mPaint.breakText(strParagraph, true, mVisibleWidth,
+                        null);
+                lines.add(strParagraph.substring(0, nSize));
+                strParagraph = strParagraph.substring(nSize);
+                if (lines.size() >= mLineCount) {
+                    break;
+                }
+            }
+            if (strParagraph.length() != 0) {
+                try {
+                    m_mbBufEnd -= (strParagraph + strReturn)
+                            .getBytes(m_strCharsetName).length;
+                } catch (UnsupportedEncodingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        return lines;
+    }
+
+    protected void pageUp() {
+        if (m_mbBufBegin < 0) {
+            m_mbBufBegin = 0;
+        }
+        Vector<String> lines = new Vector<String>();
+        String strParagraph = "";
+        while (lines.size() < mLineCount && m_mbBufBegin > 0) {
+            Vector<String> paraLines = new Vector<String>();
+            byte[] paraBuf = readParagraphBack(m_mbBufBegin);
+            m_mbBufBegin -= paraBuf.length;
+            try {
+                strParagraph = new String(paraBuf, m_strCharsetName);
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            strParagraph = strParagraph.replaceAll("\r\n", "");
+            strParagraph = strParagraph.replaceAll("\n", "");
+
+            if (strParagraph.length() == 0) {
+                paraLines.add(strParagraph);
+            }
+            while (strParagraph.length() > 0) {
+                int nSize = mPaint.breakText(strParagraph, true, mVisibleWidth,
+                        null);
+                paraLines.add(strParagraph.substring(0, nSize));
+                strParagraph = strParagraph.substring(nSize);
+            }
+            lines.addAll(0, paraLines);
+        }
+        while (lines.size() > mLineCount) {
+            try {
+                m_mbBufBegin += lines.get(0).getBytes(m_strCharsetName).length;
+                lines.remove(0);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        m_mbBufEnd = m_mbBufBegin;
+        return;
+    }
+
+    protected void prePage() throws IOException {
+        if (m_mbBufBegin <= 0) {
+            m_mbBufBegin = 0;
+            m_isfirstPage = true;
+            return;
+        } else {
+            m_isfirstPage = false;
+        }
+        m_lines.clear();
+        pageUp();
+        m_lines = pageDown();
+    }
+
+    public void nextPage() throws IOException {
+        if (m_mbBufEnd >= m_mbBufLen) {
+            m_islastPage = true;
+            return;
+        } else {
+            m_islastPage = false;
+        }
+        m_lines.clear();
+        m_mbBufBegin = m_mbBufEnd;
+        m_lines = pageDown();
+    }
+
+    public void onDraw(Canvas c) {
+        if (m_lines.size() == 0) {
+            m_lines = pageDown();
+        }
+        if (m_lines.size() > 0) {
+            if (m_book_bg == null) {
+                c.drawColor(m_backColor);
+            } else {
+                c.drawBitmap(m_book_bg, 0, 0, null);
+            }
+            int y = marginHeight;
+            for (String strLine : m_lines) {
+                y += m_fontSize;
+                c.drawText(strLine, marginWidth, y, mPaint);
+            }
+        }
+        float fPercent = (float) (m_mbBufBegin * 1.0 / m_mbBufLen);
+        mPercent = (int) (fPercent * 100);
+        String strPercent = df.format(fPercent * 100) + "%";
+        int nPercentWidth = (int) mPaint.measureText("999.9%") + 1;
+        c.drawText(strPercent, mWidth - nPercentWidth, mHeight, mPaint);
+        Editor edit = sharedPreferences.edit();
+        edit.putInt("Begin", m_mbBufBegin);
+        edit.commit();
+    }
+
+    public int getmPercent() {
+        return mPercent;
+    }
+    
+    public void setPaddingButtom(int paddingButtom) {
+        Editor edit = sharedPreferences.edit();
+        edit.putInt("PaddingButtom", paddingButtom);
+        edit.commit();
+    }
+
+    public void setmPercent(int mPercent) {
+        this.mPercent = mPercent;
+        m_mbBufEnd = m_mbBufBegin = (int) ((float) (m_mbBufLen * 1.0 * mPercent) / 100);
+        Editor edit = sharedPreferences.edit();
+        edit.putInt("Begin", m_mbBufBegin);
+        edit.commit();
+    }
+
+    public void setBgBitmap(Bitmap BG) {
+        m_book_bg = BG;
+    }
+
+    public boolean isfirstPage() {
+        return m_isfirstPage;
+    }
+
+    public boolean islastPage() {
+        return m_islastPage;
+    }
+}
---
+++ ebook/src/com/easyhome/ebook/EbookActivity.java
@@ -0,0 +1,313 @@
+
+package com.easyhome.ebook;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mobclick.android.MobclickAgent;
+import com.easyhome.ebook.R;
+
+public class EbookActivity extends Activity {
+
+    public static final String TAG = "EbookActivity";
+    public static final int OPTION_MENU_SETTING_FONT_SIZE = 0;
+    public static final int OPTION_MENU_SETTING_BACK_COLOR = 1;
+    public static final int OPTION_MENU_PAGE_SLIDE = 2;
+    private PageWidget mPageWidget;
+    Bitmap mCurPageBitmap, mNextPageBitmap;
+    Canvas mCurPageCanvas, mNextPageCanvas;
+    BookPageFactory pagefactory;
+
+    private int mScreenWidth;
+    private int mScreenHeight;
+
+    private ViewGroup viewGroup;
+
+    private static final String SOURCE_FILE_NAME = "temp.txt";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // umeng自动更新
+        MobclickAgent.update(this);
+        MobclickAgent.setUpdateOnlyWifi(false);
+
+        removePaddingButtom();
+        getDisplay();
+        setWindowStyle();
+
+        mCurPageBitmap = Bitmap.createBitmap(mScreenWidth, mScreenHeight,
+                Bitmap.Config.ARGB_8888);
+        mNextPageBitmap = Bitmap.createBitmap(mScreenWidth, mScreenHeight,
+                Bitmap.Config.ARGB_8888);
+
+        mCurPageCanvas = new Canvas(mCurPageBitmap);
+        mNextPageCanvas = new Canvas(mNextPageBitmap);
+
+        mPageWidget.setBitmaps(mCurPageBitmap, mCurPageBitmap);
+        mPageWidget.setOnTouchListener(pageWidgetOnTouchLsn());
+    }
+
+    private void removePaddingButtom() {
+        // TODO Auto-generated method stub
+        Editor editor = PreferenceManager
+                .getDefaultSharedPreferences(this).edit();
+        editor.remove("PaddingButtom");
+        editor.commit();
+    }
+
+    private File getSourceTextFile() throws IOException {
+        File tempFile = this.getCacheDir().getAbsoluteFile();
+        if (!tempFile.exists()) {
+            tempFile.mkdirs();
+        }
+        tempFile = new File(tempFile.getAbsolutePath(), SOURCE_FILE_NAME);
+        if (!tempFile.exists()) {
+            tempFile.createNewFile();
+            this.copyFile(this.getAssets().open(SOURCE_FILE_NAME), tempFile);
+        }
+        return tempFile;
+    }
+
+    private OnTouchListener pageWidgetOnTouchLsn() {
+        return new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+
+                boolean ret = false;
+                if (v == mPageWidget) {
+                    if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                        // if (e.getX() > mScreenWidth / 3 && e.getX() <
+                        // mScreenWidth / 3 * 2
+                        // && e.getY() > mScreenHeight / 3 && e.getY() <
+                        // mScreenHeight / 3 * 2) {
+                        // initPopupWindow();
+                        // return false;
+                        // }
+                        mPageWidget.abortAnimation();
+                        mPageWidget.calcCornerXY(e.getX(), e.getY());
+
+                        pagefactory.onDraw(mCurPageCanvas);
+                        if (mPageWidget.DragToRight()) {
+                            try {
+                                pagefactory.prePage();
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+                            if (pagefactory.isfirstPage())
+                                return false;
+                            pagefactory.onDraw(mNextPageCanvas);
+                        } else {
+                            try {
+                                pagefactory.nextPage();
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+                            if (pagefactory.islastPage()) {
+                                return false;
+                            }
+                            pagefactory.onDraw(mNextPageCanvas);
+                        }
+                        mPageWidget.setBitmaps(mCurPageBitmap, mNextPageBitmap);
+                    }
+
+                    ret = mPageWidget.doTouchEvent(e);
+                    return ret;
+                }
+
+                return false;
+            }
+        };
+    }
+
+    private void setWindowStyle() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mPageWidget = new PageWidget(this, mScreenWidth, mScreenHeight);
+        // setContentView(mPageWidget);
+        setContentView(R.layout.main);
+        viewGroup = (ViewGroup) findViewById(R.id.book);
+    }
+
+    // 复制文件
+    public void copyFile(InputStream srcInputStream, File targetFile)
+            throws IOException {
+        InputStream inBuff = null;
+        BufferedOutputStream outBuff = null;
+        try {
+            // 新建文件输入流并对它进行缓冲
+            inBuff = srcInputStream;
+            // 新建文件输出流并对它进行缓冲
+            outBuff = new BufferedOutputStream(new FileOutputStream(targetFile));
+            // 缓冲数组
+            byte[] b = new byte[1024 * 5];
+            int len;
+            while ((len = inBuff.read(b)) != -1) {
+                outBuff.write(b, 0, len);
+            }
+            // 刷新此缓冲的输出流
+            outBuff.flush();
+        } finally {
+            // 关闭流
+            if (inBuff != null)
+                inBuff.close();
+            if (outBuff != null)
+                outBuff.close();
+        }
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+        goOnShowDraw();
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+    private void getDisplay() {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        mScreenWidth = dm.widthPixels;
+        mScreenHeight = dm.heightPixels;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        menu.add(0, OPTION_MENU_SETTING_FONT_SIZE, 0, getString(R.string.font_size));
+        menu.add(0, OPTION_MENU_SETTING_BACK_COLOR, 0, getString(R.string.back_color));
+        menu.add(0, OPTION_MENU_PAGE_SLIDE, 0, getString(R.string.page_slide));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case OPTION_MENU_SETTING_FONT_SIZE:
+                Intent intent = new Intent(this, SettingFont.class);
+                startActivity(intent);
+                break;
+            case OPTION_MENU_SETTING_BACK_COLOR:
+                Intent intent1 = new Intent(this, SettingBackColor.class);
+                startActivity(intent1);
+                break;
+            case OPTION_MENU_PAGE_SLIDE:
+                initPopupWindow();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initPopupWindow() {
+        // TODO Auto-generated method stub
+        PopupWindow popup = new PopupWindow(EbookActivity.this);
+
+        popup.setContentView(getPopupView());
+
+        popup.setFocusable(true);
+
+        popup.setBackgroundDrawable(getResources().getDrawable(R.drawable.page_slide_bg));
+
+        popup.setWidth(mScreenWidth);
+
+        popup.setHeight(120);
+
+        popup.showAtLocation(mPageWidget, Gravity.BOTTOM, 0, 0);
+    }
+
+    private View getPopupView() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View popView = layoutInflater.inflate(R.layout.page_slide, null);
+
+        final TextView textView = (TextView) popView.findViewById(R.id.textsize);
+        textView.setText(pagefactory.getmPercent() + "%");
+
+        SeekBar seekBar = (SeekBar) popView.findViewById(R.id.seekBar);
+        seekBar.setProgress(pagefactory.getmPercent());
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+                pagefactory.setmPercent(seekBar.getProgress());
+                goOnShowDraw();
+                try {
+                    pagefactory.nextPage();
+                    pagefactory.prePage();
+                    pagefactory.onDraw(mCurPageCanvas);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // TODO Auto-generated method stub
+                textView.setText(progress + "%");
+            }
+        });
+
+        return popView;
+    }
+
+    private void goOnShowDraw() {
+        // TODO Auto-generated method stub
+        mPageWidget.setBitmaps(mCurPageBitmap, mCurPageBitmap);
+        // setContentView(mPageWidget);
+        viewGroup.removeView(mPageWidget);
+        viewGroup.addView(mPageWidget);
+
+        pagefactory = new BookPageFactory(mScreenWidth, mScreenHeight, this);
+        // Bitmap bm = null;
+        // pagefactory.setBgBitmap(bm);
+        try {
+            File tempFile = getSourceTextFile();
+            pagefactory.openbook(tempFile);
+            pagefactory.onDraw(mCurPageCanvas);
+        } catch (IOException e) {
+            Toast.makeText(this, R.string.open_failed_msg, Toast.LENGTH_LONG)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+}
